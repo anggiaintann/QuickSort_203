c74eaed295df392a0818a0ce887ce37d39cd437e
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-03-31 18:54:51 +0700

    membuat void while

--- QuickSort2/QuickSort2.cpp
+++ QuickSort2/QuickSort2.cpp
@@ -6,3 +6,13 @@ int arr[20];
 int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of movement
 int n;
+void input() {
+	while (true) {
+		cout << "masukkan panjang element array :";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\n maksimum panjang array adalah 20" << endl;
+	}
